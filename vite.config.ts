9e26ba5fa92773183a236b75c1053fd74fb4eb2c
iqlip <122716618+iqlipx@users.noreply.github.com>
2025-03-10 12:52:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  server: {
-    historyApiFallback: {
-      index: '/', // This is the key part for fallback to the root.
-    },
   },
 });
