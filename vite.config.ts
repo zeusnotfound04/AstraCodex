144ca624a76bfc30b7a184f14d2fdaaa641babff
iqlip <122716618+iqlipx@users.noreply.github.com>
2025-03-10 12:18:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/', // Reset this to default behavior
+  server: {
+    historyApiFallback: true,
+  },
 });
