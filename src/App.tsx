d82dc046c5cc7b74c6f38e7046c1a197b6be780f
iqlip <122716618+iqlipx@users.noreply.github.com>
2025-03-13 01:07:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Menu } from "lucide-react";
 import { Sidebar } from "./components/Sidebar";
 import { SearchBar } from "./components/SearchBar";
@@ -9,7 +13,6 @@ import { categories } from "./data/categories";
 import { resources } from "./data/resources";
 import { Resource, Category } from "./types";
 import Landing from "./components/Landing";
-import { Analytics } from "@vercel/analytics/react";  // Import Analytics
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -40,6 +43,7 @@ function App() {
           collectedResources = collectedResources.concat(collectResources(subcategory));
         }
       }
+
       return collectedResources;
     };
 
@@ -158,7 +162,6 @@ function App() {
                 </Routes>
               </main>
             </div>
-            <Analytics />  
           </div>
         } />
       </Routes>
